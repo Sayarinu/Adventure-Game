958af4cb475736a8d0aec7c193de9217181d64ce
Nick Pham <hebenzudoaa@gmail.com>
2022-10-05 16:15:59 -0400

    chargers

--- Adventure Game/Assets/Scripts/BotCode.cs
+++ Adventure Game/Assets/Scripts/BotCode.cs
@@ -3,23 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class BotCode : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    GameObject player;
+    GameObject enemy;
 
+    public int enemyType;
+    /*
+    0 - default charger
+    1 - faster charger
+    2 - zooming charger (usually goes too fast to hit if you dodge bc inertia)
+    3 - shooter
+    4 - circler
+    5 - tank
+    */
     NavMeshAgent _agent;
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        player = GameObject.FindGameObjectWithTag("Player");
+        enemy = GameObject.FindGameObjectWithTag("Player");
         StartCoroutine(LookForPlayer());
     }
     IEnumerator LookForPlayer() {
         while (true) {
-            yield return new WaitForSeconds(.5f);
-            _agent.SetDestination(player.transform.position);
+            switch(enemyType){
+                case 0:
+                    _agent.speed=1;
+                    yield return new WaitForSeconds(.5f);
+                    _agent.SetDestination(enemy.transform.position);
+                    break;
+                case 1:
+                    _agent.speed=5;
+                    yield return new WaitForSeconds(.5f);
+                    _agent.SetDestination(enemy.transform.position);
+                    break;
+                case 2:
+                    _agent.speed=10;
+                    yield return new WaitForSeconds(.5f);
+                    _agent.SetDestination(enemy.transform.position);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
