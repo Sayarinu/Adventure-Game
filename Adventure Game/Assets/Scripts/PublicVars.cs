67c80add6798cf34c7c042794da32b7759aafeb2
Sayarinu <bikabattles@gmail.com>
2022-10-06 22:05:20 -0400

    Update PublicVars.cs

--- Adventure Game/Assets/Scripts/PublicVars.cs
+++ Adventure Game/Assets/Scripts/PublicVars.cs
@@ -2,5 +2,5 @@ public static class PublicVars {
     public static int keys = 0;
     public static int currScenePos = 0;
 
-    public static int enemiesLevel3 = 7;
+    public static int enemiesLevel3 = 14;
 }
